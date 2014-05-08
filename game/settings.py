c31189922e79da9f485aebaa03edfd4113e11678
asechrest <sechrest.adam@gmail.com>
2014-05-08 01:44:56 -0400

    Fleshing out a chapter object, a book object, and associated commands.

---
+++ game/settings.py
@@ -0,0 +1,27 @@
+
+######################################################################
+# Evennia MU* server configuration file
+#
+# You may customize your setup by copy&pasting the variables you want
+# to change from the master config file src/settings_default.py to
+# this file. Try to *only* copy over things you really need to customize
+# and do *not* make any changes to src/settings_default.py directly.
+# This way you'll always have a sane default to fall back on
+# (also, the master config file may change with server updates).
+#
+######################################################################
+
+from src.settings_default import *
+
+######################################################################
+# Custom settings
+######################################################################
+
+
+######################################################################
+# SECRET_KEY was randomly seeded when settings.py was first created.
+# Don't share this with anybody. It is used by Evennia to handle
+# cryptographic hashing for things like cookies on the web side.
+######################################################################
+SECRET_KEY = 'y,/H(fa&[FnVXZT6DC*lwj~O{>pEx5g?+M$S`L|<'
+
